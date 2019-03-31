6e01c76887a9c520f8d2a584354fd5c1b344910e
Ben Daniels <benbanj@hotmail.com>
2019-03-31 14:10:57 +0100

    Finishing main logic

--- script.js
+++ script.js
@@ -1,41 +1,66 @@
-let count = 16;
-let container = document.querySelector('#grid-container');
-container.style.gridTemplateColumns = `repeat(${count}, auto)`;
-container.style.gridTemplateRows = `repeat(${count}, auto)`;
-/*Create grid*/
-for (let i = 0; i < count ** 2; i++) {
-  let divvy = document.createElement('div');
-  container.appendChild(divvy);
+let sqCount = 16;
+
+setSides()
+
+function setSides() {
+  let sqButton = document.querySelector('#sq-button');
+  sqButton.addEventListener('click', function(){
+    let num = prompt("How many squares per side?");
+    deleteGrid();
+    createGrid(num);
+  });
+}
+
+
+function createGrid(numberOfSquares) {
+  let sides = Number(numberOfSquares);
+  let container = document.querySelector('#grid-container');
+  container.style.gridTemplateColumns = `repeat(${sides}, auto)`;
+  container.style.gridTemplateRows = `repeat(${sides}, auto)`;
+  /*Create grid*/
+  for (let i = 0; i < sides ** 2; i++) {
+    let divvy = document.createElement('div');
+    container.appendChild(divvy);
+  }
+  colorDivs();
+  clear();
+}
+createGrid(sqCount)
+
+function deleteGrid() {
+  let container = document.querySelector('#grid-container');
+  while (container.lastChild) {
+    container.removeChild(container.lastChild);
+  }
 }
 
 /*Apply color to divs*/
-let divs = Array.from(document.querySelectorAll('#grid-container > div'));
-divs.forEach(function(item){
-  item.addEventListener('mouseover', function(e){
-    // let current_color = this.style.backgroundColor;
-    let current_color = this.style.backgroundColor;
-    if (current_color) {
-      let colors = current_color.match(/\d+/g);
-      for (let i = 0; i < 3; i++) {
-        if (+colors[i] > 25) {
-          colors[i] = +colors[i] - 25;
-        } else {
-          colors[i] = 0;
+function colorDivs() {
+  let divs = Array.from(document.querySelectorAll('#grid-container > div'));
+  divs.forEach(function(item){
+    item.addEventListener('mouseover', function(e){
+      let current_color = this.style.backgroundColor;
+      if (current_color) {
+        let colors = current_color.match(/\d+/g);
+        for (let i = 0; i < 3; i++) {
+          if (+colors[i] > 25) {
+            colors[i] = +colors[i] - 25;
+          } else {
+            colors[i] = 0;
+          }
         }
+        let red;
+        let green;
+        let blue;
+        [red, green, blue] = colors
+        let new_shade = `rgb(${red}, ${green}, ${blue})`;
+        this.style.backgroundColor = new_shade;
+      } else {
+          this.style.backgroundColor = randomColor();
       }
-      let red;
-      let green;
-      let blue;
-      [red, green, blue] = colors
-      let new_shade = `rgb(${red}, ${green}, ${blue})`;
-      // console.log(current_color);
-      // console.log(new_shade);
-      this.style.backgroundColor = new_shade;
-    } else {
-        this.style.backgroundColor = randomColor();
-    }
+    });
   });
-});
+}
 
 function randomColor(){
   let arr = [];
@@ -47,8 +72,9 @@ function randomColor(){
 }
 randomColor();
 
-/*Clear grid*/
+/*Clear grid of color*/
 function clear() {
+  let divs = Array.from(document.querySelectorAll('#grid-container > div'));
   let clearBtn = document.querySelector('#clear-button');
   clearBtn.addEventListener('click', function(){
     divs.forEach(function(item){
